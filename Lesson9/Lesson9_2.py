944e91b9c48da1c39664f4625295dae229260eb4
Sharovarov Aleksandr <sharovarovaleksandr@gmail.com>
2022-11-21 15:40:15 +0200

    Add DZ Lesson9

--- Lesson9/Lesson9_2.py
+++ Lesson9/Lesson9_2.py
@@ -2,58 +2,81 @@ class TextProcessor():
 
     set_punctuation = (".", ",", ":", ";", "!", "?", "-")
 
-    # def __init__(self,__text_processor):
-    #     self.__text_processor=text_processor
+    def __init__(self,text_processor):
+        self.text_processor=text_processor
 
 # Варіант 1  Коли приватний метод классу __is_punctuation повертає приватний аргумент True/False
     @classmethod
     def get_clean_string(cls,s):
         s1=""
         for i in list(s):
-            if TextProcessor.__is_punctuation(i,False):
+            if TextProcessor.__is_punctuation(i):
                 continue
             else:
                 s1 += i
         return s1
 
     @classmethod
-    def __is_punctuation(cls, s, b):
+    def __is_punctuation(cls, s):
         if s in TextProcessor.set_punctuation:
             b= True
         else:
             b= False
         return b
 
-#Ваіант 2 Коли приватний метод класу __is_punctuation обробляє запрос True/False всередині класу
-    # @classmethod
-    # def get_clean_string(cls,s):
-    #     s1=""
-    #     for i in list(s):
-    #         if TextProcessor.__is_punctuation(i):
-    #             continue
-    #         else:
-    #             s1 += i
-    #     return s1
-    #
-    # @classmethod
-    # def __is_punctuation(cls, s):
-    #     if s in TextProcessor.set_punctuation:
-    #         return  True
-    #     else:
-    #         return  False
-
-# class TextLoader(TextProcessor):
 #
-#     def __init__(self,__clean_string):
-#         self.__clean_string=__clean_string
-#
-#     @classmethod
-#     def set_clean_text(cls,text_processor):
+
+class TextLoader(TextProcessor):
+
+    def __init__(self,text_processor,clean_string):
+        self.__text_processor=TextProcessor.text_processor
+        self.__clean_string=clean_string
+
+    @property
+    def text_processor(self):
+        return self.__text_processor
+
+    @text_processor.setter
+    def text_processor(self,value):
+        self.__text_processor=value
+
+    @property
+    def clean_string(self):
+        return self.__clean_string
+
+    @property
+    def clean_string(self):
+        print("Виводимо  текст очищений від знаків пуктуації")
+        return
+
+    @clean_string.setter
+    def clean_string(self, value):
+        self.__clean_string = value
+
+
+    @classmethod
+    def set_clean_text(cls):
+        return text_processor()
+
+class DataInterface(TextLoader):
+    # def __init__(self,atribut):
+    #     self.__atribut=
+
+    @classmethod
+    def processor_texts(cls,lst):
+        for i in lst:
+            print(TextProcessor.get_clean_string(i))
+        return
 
 
 
 
-st=" Ну, як можливо це зрозуміти? Тут повинен бути - знак, який я повинен видалити !"
-print(st)
-print(TextProcessor.get_clean_string(st))
+# st=" Ну, як можливо це зрозуміти? Тут повинен бути - знак, який я повинен видалити !"
+# print(st)
+# print(TextProcessor.get_clean_string(st))
 
+lst=[" Ну, як можливо це зрозуміти? Тут повинен бути - знак, який я повинен видалити !",
+     " Це друге, вже вдруге, речення для тесту!?",
+     " А, може бути, ще одно речення!! "]
+
+DataInterface.processor_texts(lst)
