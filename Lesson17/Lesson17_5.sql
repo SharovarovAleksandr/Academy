07d2e88a766ad822e503d21ad9df22edadd6a873
Sharovarov Aleksandr <sharovarovaleksandr@gmail.com>
2022-12-13 11:35:49 +0200

    Add Lesson 17

--- Lesson17/Lesson17_5.sql
+++ Lesson17/Lesson17_5.sql
@@ -1,4 +1,11 @@
 USE pds;
-SELECT  employees.first_name, employees.last_name, departments.department_name, departments.department_id, locations.city
-FROM employees, departments, locations
-WHERE employees.department_id=departments.department_id AND departments.location_id=locations.location_id AND locations.city='London';
+--SELECT  employees.first_name, employees.last_name, departments.department_name, departments.department_id, locations.city
+--FROM employees, departments, locations
+--WHERE employees.department_id=departments.department_id AND departments.location_id=locations.location_id AND locations.city='London';
+
+
+-- Інший спосіб
+SELECT employees.first_name, employees.last_name, departments.department_name, departments.department_id, locations.city
+FROM employees
+JOIN departments ON employees.department_id=departments.department_id
+JOIN  locations ON departments.location_id=locations.location_id AND locations.city='London';
