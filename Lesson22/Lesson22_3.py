eb67426a71a83a76e1203601bd133286b72995d8
Sharovarov Aleksandr <sharovarovaleksandr@gmail.com>
2023-01-08 16:07:41 +0200

    Add Lesson 22

--- Lesson22/Lesson22_3.py
+++ Lesson22/Lesson22_3.py
@@ -1,26 +1,30 @@
-A={0, 3, 4, 5, 7, 9,11, 18, 47}
+A={0, 3, 4, 5, 7, 9, 11, 18, 47}
 B={5, 7, 47}
-print(A in B)
+print("A>B",B)
+print("A<B",A<B)
+print("A>=B",A>=B)
+print("A<=B",A<=B)
+print("A==B",A==B)
+print("A!=B",A!=B)
+print("A^B",A^B)
+print("A & B",A&B)
+print("A - B",A-B)
+print("B - A",B-A)
+
+
+A={3, 6, 7, 8, 10, 12, 14, 21, 50}
+B={8, 10, 50,61}
+
+print(3 in A)
+print(8 not in B)
+print(A.issubset(B))
+print(B.issubset(A))
+print(A.issuperset(B))
+print(A.difference(B))
+print(A.symmetric_difference(B))
+print(A.isdisjoint(B))
 print(A.union(B))
 print(A.intersection(B))
-print(A-B)
 A.update(B)
 print(A)
 
-# A={3, 6, 7, 8, 10, 12, 14, 21, 50}
-# B={8, 10, 50}
-# print(A in B)
-# print(A.union(B))
-# print(A.intersection(B))
-# print(A-B)
-# A.update(B)
-# print(A)
-#
-# A={0, 8, 14, 18, 21, 22, 25, 30, 55}
-# B={0, 22, 55}
-# print(A in B)
-# print(A.union(B))
-# print(A.intersection(B))
-# print(A-B)
-# A.update(B)
-# print(A)
